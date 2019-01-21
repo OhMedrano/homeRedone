b8b21f12cea171803ea5e440ec0e317ab2e1376b
Oscar <omedrano564@gmail.com>
2019-01-21 11:38:08 -0500

    styling changes

--- src/PageSections/Gallery.js
+++ src/PageSections/Gallery.js
@@ -23,25 +23,7 @@ function GetImgurData() {
 
 
 
-/*function galleryCarousel(imageContainer, imagesDisplayedDesktop, currentPage) {
-  const carouselContainer = makeEle.createEle('div','galleryCarouselParent', [12,12,12,12],'galleryCarouselContainer');
-  let results = Array(imagesDisplayedDesktop).fill(null);
-  let offset = imagesDisplayedDesktop * currentPage;
-  console.log(results);
 
-
-
-
-
-
-
-
-
-
-  return carouselContainer;
-
-
-}*/
 
 
 
@@ -51,11 +33,12 @@ class GalleryCarousel {
     this.state = {
       currentCount: imageCount,
       currentPage: currentPage? currentPage : 0,
-      totalPages: images ? Math.floor(images.length / imageCount) : 0,
+      totalPages: images ? Math.ceil(images.length / imageCount) : 0,
       images: images ? images : [],
       renderedImages: [],
     }
     this.changePage = this.changePage.bind(this);
+    this.displayModal = this.displayModal.bind(this); 
   }
 
 
@@ -63,16 +46,22 @@ class GalleryCarousel {
     let startCount = this.state.currentCount * currentPage; 
     let endCount = startCount + this.state.currentCount;
     let imageResults = [];
-    for(startCount; startCount <= endCount; startCount++) {
+    for(startCount; startCount < endCount; startCount++) {
+      console.log(startCount);
       let currentImage = this.state.images[startCount];
-      console.log(currentImage,startCount);
       if(startCount >= this.state.images.length) {
         console.log('No image exist');
       } else {       
-        let renderImageContainer = makeEle.createEle('div','renderImageContainer_'+(endCount - startCount), [12,12,12,3], 'renderImageContainer');
+        let renderImageContainer = makeEle.createEle('div','renderImageContainer_'+(endCount - startCount), [3,6,12,2], 'renderImageContainer');
         let renderImage = makeEle.createEle('div','renderImage_'+(endCount - startCount), null,'renderImages');
         renderImage.style.background = 'url('+currentImage.link+') no-repeat';
+        renderImage.startCount = startCount;
         renderImageContainer.append(renderImage);
+        
+        renderImage.addEventListener('click', () => {
+          this.displayModal(currentImage);
+        });
+
         imageResults.push(renderImageContainer);
 
       }
@@ -83,35 +72,70 @@ class GalleryCarousel {
 
 
 
+  displayModal(picture) {
+    let displayedModal = makeEle.createEle('div','displayModal', [12,12,12,12],'displayModalContainer');
+    let displayPicture = makeEle.createEle('img','displayPicture', [12,12,12,12],'displayModalPicture');
+    let pictureInfo = makeEle.createEle('div','picInfo',[12,12,12,12],'picInfo');
+
+    displayPicture.src = picture.link;
+    displayPicture.title = picture.description ? picture.description : '';
+    displayedModal.append(displayPicture);
+
+
+
+
+
+
+    document.querySelector('body').append(displayedModal);
+
+    displayedModal.addEventListener('click', (e) => {
+      if(e.target.classList.contains('displayModalContainer')) {
+        displayedModal.remove();
+      }
+    })
+  }
+
+
+
 
 
   changePage(pageTurn) {
     let currentPage = this.state.currentPage;
+    let newImages = [];
+    let loadedSet = false; 
+
     if(pageTurn) {
-      if((currentPage + 1) > this.state.totalPages ) {
+      if((currentPage + 1) >= this.state.totalPages ) {
         console.log('max page reached');
+
       } else {
         this.state.currentPage = currentPage + 1;
         this.imageSets(this.state.currentPage);
+
+
       }
     } else {
-      if((currentPage - 1) < 0) {
+      if((currentPage-1) < 0) {
         console.log('first page reached');
+
       } else {
         this.state.currentPage = currentPage - 1;
         this.imageSets(this.state.currentPage);
+   
       }
     }
-    this.rerender();
+    newImages = this.state.renderedImages;
+
+    document.querySelector('#imageContainer').innerHTML = '';
+
+    newImages.forEach((img, i) => {
+       document.querySelector('#imageContainer').append(img);
+    })
+    
   }
 
-
-  rerender() {
-    this.renderDiv();
-  }
 
   renderDiv(imageCount, ) {
-    console.log(this.state);
     this.imageSets(this.state.currentPage);
     let carouselContainerWithControls = makeEle.createEle('div','carouselWrapper',[12,12,12,12],'carouselWrapper');
 
@@ -122,21 +146,52 @@ class GalleryCarousel {
     let imagesArray = this.state.renderedImages; 
 
     imageControlCount.forEach((ctr, i) => {
-
+      let controlIcon = makeEle.createEle('div','controlIcon_'+i, null,'controlIcon');
       let leftOrRight = makeEle.createEle('div','imageControl_'+i,null,'imageControls');
+       if(i == 0) {
+        controlIcon.innerHTML = '<'; 
+        if(this.state.currentPage == 0) {
+          leftOrRight.classList.add('invalid');
+        } else {
+          leftOrRight.classList.remove('invalid');
+        }
+      } else {
+        controlIcon.innerHTML = '>'; 
+      }
+
       leftOrRight.addEventListener('click', (e) => {
          let currentPage = this.state.currentPage;
+             
           if(i == 1) {
             this.changePage(true)
+            document.querySelector('#imageControl_0').classList.remove('invalid');
+            if((currentPage + 2) >= this.state.totalPages) {
+              leftOrRight.classList.add('invalid');
+            }
           } else {
             this.changePage(false);
+            document.querySelector('#imageControl_1').classList.remove('invalid');
+            if(this.state.currentPage == 0) {
+              leftOrRight.classList.add('invalid');
+            } else {
+             leftOrRight.classList.remove('invalid'); 
+            }
           }
       })
+
+
+      /*
+
+        Need to re-factor this cause it's too much repeating. 
+
+        Will redo later. Rough draft first.
+
+
+      */
+      leftOrRight.append(controlIcon);
       imageControl.push(leftOrRight);
+
     })
-
-
-
 
     imagesArray.forEach((image, i) => {
       imageContainer.append(image);
@@ -161,9 +216,10 @@ class GalleryCarousel {
 function GalleryPage(galleryPics) {
   let imageResults;
   const galleryPageContainer = makeEle.createEle('div','galleryPageContainer',[12,12,12,12],['bodyContentItem','galleryPageContainer']);
+  let galleryTitle = makeEle.createEle('div','galleryTitle',[12,12,12,12],'galleryTitle');
   let loadedState = false; 
   let state = {
-    imageCount: 8,
+    imageCount: 12,
     imageArray: [],
 
   }
@@ -176,16 +232,15 @@ function GalleryPage(galleryPics) {
         if(Array.isArray(imageResults)){
           loadedState != loadedState;
           let totalPages =  imageResults ? Math.floor(imageResults.length / state.imageCount) : 0;
-          let galleryCarousel = new GalleryCarousel(7, 0, imageResults);        
-          galleryPageContainer.append(galleryCarousel.renderDiv());
+          let galleryCarousel = new GalleryCarousel((state.imageCount), 0, imageResults);
+
+          galleryTitle.innerHTML = `<div class='titleText'> <div class='text_1'>Real life drawings</div> <div class='text_2'> Check more out on my <a href='http://instagram.com/the_big_oh'> Instagram </a> </div> </div>`;
+
+          galleryPageContainer.append(galleryTitle,galleryCarousel.renderDiv());
         }
 
     })
   }
-  
-
-
-
 
   return galleryPageContainer;
 
